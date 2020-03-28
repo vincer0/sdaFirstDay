16397de93450166489aa1e85f3857e68fa6be37f
Vincer0 <pasierb.adam@gmail.com>
2020-03-28 11:14:52 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java 17 ");
+        System.out.println("Hello Java 18 ");
+        System.out.println("Hello Java 19 ");
+        System.out.println("Hello Java 20 ");
+    }
+}
